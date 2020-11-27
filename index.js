cfe19bca69f3af15480cee42feffa5979d42baa7
michael webster <michael.webster@motusdesign.ca>
2020-11-27 13:23:15 -0800

    reducing diagnostic dumps

--- index.js
+++ index.js
@@ -135,29 +135,29 @@ async function action (){
     token: null
   };
 
-  console.log(JSON.stringify(stages));
+  // console.log(JSON.stringify(stages));
 
   try {
-    console.log("config: " + JSON.stringify(repoConfig));
     repoConfig.id = core.getInput('repo-id', {required: true});
     repoConfig.name = core.getInput('repo-name', {required: true});
     repoConfig.owner = core.getInput('repo-owner', {required: true});
     repoConfig.token = core.getInput('repo-token', {required: true});
-    console.log("config: " + JSON.stringify(repoConfig));
+    console.log("repo:\n" + JSON.stringify(repoConfig));
     
     var projectName = core.getInput('project-name', {required: true});
     var projectBody = core.getInput('project-body', {required: true});
-    console.log(projectName + "  " + projectBody);
+    // console.log(projectName + "  " + projectBody);
     
     const resultProject = await addRepoProject(repoConfig, projectName, projectBody);
-    console.log("project result: ", JSON.stringify(resultProject));
+    // console.log("project result: ", JSON.stringify(resultProject));
     
     var projID = resultProject.id;
-    console.log(projID);
+    // console.log(projID);
     for(const stage of stages) {
         const resultColumn = await addProjectColumn(repoConfig, resultProject.id, stage.name)
-        console.log("project result: ", JSON.stringify(resultColumn));               
+        // console.log("project result: ", JSON.stringify(resultColumn));               
     }
+    console.log("final result:\n", JSON.stringify(resultColumn));               
   } catch (err) {
       console.log("failed", err.request)
       console.log(err.message)
